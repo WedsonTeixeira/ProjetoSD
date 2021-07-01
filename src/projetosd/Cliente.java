0dd9345324280e453e7658cdbb472f93178396a9
WedsonTeixeira <wedson.teixeira1998@outlook.com>
2021-06-30 23:10:32 -0300

    Codigo para enviar nome de arquivo e receber

--- src/projetosd/Cliente.java
+++ src/projetosd/Cliente.java
@@ -1,5 +1,25 @@
 package projetosd;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Cliente {
-    
+
+    public static void main(String argv[]) throws IOException{
+        
+        Socket clientSocket = new Socket("localhost", 10000);
+        
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        
+        outToServer.writeBytes("arquivo" + '\n');
+        
+        String arquivo = inFromServer.readLine();
+        System.out.println(arquivo);
+        clientSocket.close();
+        
+    }
 }
--- src/projetosd/Servidor.java
+++ src/projetosd/Servidor.java
@@ -1,5 +1,28 @@
 package projetosd;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
-    
+    public static void main(String argv[]) throws Exception {
+        
+       ServerSocket welcomeSocket = new ServerSocket(10000);
+        
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            
+            String nomeArquivo = inFromClient.readLine();
+            
+            System.out.print(nomeArquivo);
+            outToClient.writeBytes("Aqui vai uma lista\n");
+         
+            
+        }
+    }
 }
